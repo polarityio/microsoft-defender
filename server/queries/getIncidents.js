d5fb589198345cde8781862483df8ac3d9a625b7
Jon Penwood <jonp@polarity.io>
2023-04-17 17:32:33 -0500

    feat: Added request limiter and config.json, & removed mocked request responses.

--- server/queries/getIncidents.js
+++ server/queries/getIncidents.js
@@ -17,75 +17,7 @@ const getIncidents = async (entities, options) =>
       },
       options
     })),
-    async (incidentRequests) =>
-      map(
-        (request) => ({
-          entity: request.entity,
-          result: [
-            {
-              '@odata.type': '#microsoft.graph.security.incident',
-              id: '2972395',
-              incidentWebUrl:
-                'https://security.microsoft.com/incidents/2972395?tid=12f988bf-16f1-11af-11ab-1d7cd011db47',
-              redirectIncidentId: null,
-              tenantId: 'b3c1b5fc-828c-45fa-a1e1-10d74f6d6e9c',
-              displayName:
-                'Multi-stage incident involving Initial access & Command and control on multiple endpoints reported by multiple sources',
-              createdDateTime: '2021-08-13T08:43:35.5533333Z',
-              lastUpdateDateTime: '2021-09-30T09:35:45.1133333Z',
-              assignedTo: 'jonp@polarity.io',
-              classification: 'TruePositive',
-              determination: 'MultiStagedAttack',
-              status: 'Active',
-              severity: 'Medium',
-              customTags: ['Demo'],
-              comments: [
-                {
-                  comment: 'Demo incident',
-                  createdBy: 'DavidS@contoso.onmicrosoft.com',
-                  createdTime: '2021-09-30T12:07:37.2756993Z'
-                },
-                {
-                  comment: 'Demo incident',
-                  createdBy: 'DavidS@contoso.onmicrosoft.com',
-                  createdTime: '2021-09-30T12:07:37.2756993Z'
-                },
-                {
-                  comment: 'Demo incident',
-                  createdBy: 'DavidS@contoso.onmicrosoft.com',
-                  createdTime: '2021-09-30T12:07:37.2756993Z'
-                },
-              ]
-            },
-            {
-              '@odata.type': '#microsoft.graph.security.incident',
-              id: '2972395',
-              incidentWebUrl:
-                'https://security.microsoft.com/incidents/2972395?tid=12f988bf-16f1-11af-11ab-1d7cd011db47',
-              redirectIncidentId: null,
-              tenantId: 'b3c1b5fc-828c-45fa-a1e1-10d74f6d6e9c',
-              displayName:
-                'Multi-stage incident involving Initial access & Command and control on multiple endpoints reported by multiple sources',
-              createdDateTime: '2021-08-13T08:43:35.5533333Z',
-              lastUpdateDateTime: '2021-09-30T09:35:45.1133333Z',
-              assignedTo: 'jonp@polarity.io',
-              classification: 'TruePositive',
-              determination: 'MultiStagedAttack',
-              status: 'Active',
-              severity: 'Medium',
-              customTags: ['Demo'],
-              comments: [
-                {
-                  comment: 'Demo incident',
-                  createdBy: 'DavidS@contoso.onmicrosoft.com',
-                  createdTime: '2021-09-30T12:07:37.2756993Z'
-                }
-              ]
-            }
-          ]
-        }),
-        incidentRequests
-      ) //await requestsInParallel(incidentRequests, 'body.value')
+    async (incidentRequests) => await requestsInParallel(incidentRequests, 'body.value')
   )(entities);
 
 module.exports = getIncidents;
--- server/request/createRequestWithDefaults.js
+++ server/request/createRequestWithDefaults.js
@@ -2,15 +2,25 @@ const fs = require('fs');
 
 const request = require('postman-request');
 const { get } = require('lodash/fp');
+const Bottleneck = require('bottleneck/es5');
 
 const { ERROR_MESSAGES } = require('../constants');
 const authenticateRequest = require('./authenticateRequest');
 const { getLogger } = require('../logging');
 
-const SUCCESSFUL_ROUNDED_REQUEST_STATUS_CODES = [200];
 
 const _configFieldIsValid = (field) => typeof field === 'string' && field.length > 0;
 
+let limiter;
+
+function _setupLimiter(options) {
+  limiter = new Bottleneck({
+    maxConcurrent: 10, // no more than 5 lookups can be running at single time
+    highWater: 100, // no more than 50 lookups can be queued up
+    strategy: Bottleneck.strategy.OVERFLOW,
+    minTime: 100 // don't run lookups faster than 1 every options.minTime ms
+  });
+}
 const createRequestWithDefaults = () => {
   const {
     request: { ca, cert, key, passphrase, rejectUnauthorized, proxy }
@@ -44,15 +54,17 @@ const createRequestWithDefaults = () => {
       });
 
     return async (requestOptions) => {
+      if (!limiter) _setupLimiter(requestOptions.options);
+
       const preRequestFunctionResults = await preRequestFunction(requestOptions);
       const _requestOptions = {
         ...requestOptions,
         ...preRequestFunctionResults
       };
 
-      let postRequestFunctionResults;
       try {
-        const result = await _requestWithDefaults(_requestOptions);
+        result = await limiter.schedule(_requestWithDefaults, _requestOptions);
+
         checkForStatusError(result, _requestOptions);
 
         postRequestFunctionResults = await postRequestSuccessFunction(
@@ -60,10 +72,22 @@ const createRequestWithDefaults = () => {
           _requestOptions
         );
       } catch (error) {
-        postRequestFunctionResults = await postRequestFailureFunction(
-          error,
-          _requestOptions
-        );
+        try {
+          postRequestFunctionResults = await postRequestFailureFunction(
+            error,
+            _requestOptions
+          );
+        } catch (_error) {
+          const err = parseErrorToReadableJSON(_error);
+          _error.maxRequestQueueLimitHit =
+            (isEmpty(err) && isEmpty(result)) ||
+            (err && err.message === 'This job has been dropped by Bottleneck');
+
+          _error.isConnectionReset =
+            getOr('', 'errors[0].meta.err.code', err) === 'ECONNRESET';
+          _error.entity = JSON.stringify(_requestOptions.entity);
+          throw _error;
+        }
       }
       return postRequestFunctionResults;
     };
@@ -90,13 +114,15 @@ const createRequestWithDefaults = () => {
 
     const roundedStatus = Math.round(statusCode / 100) * 100;
     const statusCodeNotSuccessful =
-      !SUCCESSFUL_ROUNDED_REQUEST_STATUS_CODES.includes(roundedStatus);
+      ![200].includes(roundedStatus);
     const responseBodyError = get('error', body);
 
     if (statusCodeNotSuccessful || responseBodyError) {
       const requestError = Error(
         `Request Error${
-          get('message', responseBodyError) ? ` -> ${get('message', responseBodyError)}` : ''
+          get('message', responseBodyError)
+            ? ` -> ${get('message', responseBodyError)}`
+            : ''
         }`
       );
       requestError.status = statusCodeNotSuccessful
@@ -109,7 +135,6 @@ const createRequestWithDefaults = () => {
     }
   };
 
-  //TODO Add limiter
   const requestDefaultsWithInterceptors = requestWithDefaultsBuilder(authenticateRequest);
 
   return requestDefaultsWithInterceptors;
